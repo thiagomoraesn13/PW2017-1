167e15b8c7313abef2062ebe753e9272eb2d5cf2
thiagomoraesn13 <tmr@icomp.ufam.edu.br>
2017-06-20 19:59:16 -0400

    modificando

--- YII/advanced/frontend/views/site/about.php
+++ YII/advanced/frontend/views/site/about.php
@@ -11,6 +11,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>This is the About page. You may modify the following file to customize its content:</p>
-
+    <p><?= $data ?></p>
     <code><?= __FILE__ ?></code>
 </div>
