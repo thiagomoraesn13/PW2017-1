13de60f836d9539116d495aeca2401ec2df7e578
thiagomoraesn13 <tmr@icomp.ufam.edu.br>
2017-07-12 13:53:16 -0400

    incluindo

---
+++ YII/advanced/common/models/Jogada.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "jogada".
+ *
+ * @property integer $int
+ * @property integer $id_user
+ * @property integer $pontuacao
+ * @property string $data_hora
+ */
+class Jogada extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'jogada';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['int', 'id_user', 'pontuacao', 'data_hora'], 'required'],
+            [['int', 'id_user', 'pontuacao'], 'integer'],
+            [['data_hora'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'int' => 'Int',
+            'id_user' => 'Id User',
+            'pontuacao' => 'Pontuacao',
+            'data_hora' => 'Data Hora',
+        ];
+    }
+}
